26b1aa90c3295d9f24ae4b341565a6abb0d02229
Inessa <breathe7underwater@gmail.com>
2022-07-26 12:36:07 +0200

    Added working current weather icon

--- src/java.js
+++ src/java.js
@@ -41,15 +41,16 @@ function fullDate () {
 }
 
 function showMainInfo(response) {
-
 console.log(response)
 
+let cityEl = document.querySelector("#city-upd");
 let tempEl = document.querySelector("#temp");
 let tempRealEl = document.querySelector("#real-feel");
 let windEl = document.querySelector("#details-wind");
 let humEl = document.querySelector("#details-hum");
 let timeEl = document.querySelector("#time-upd");
 let dayEl = document.querySelector("#date-upd");
+let iconEl = document.querySelector("#current-icon");
 
 let sunriseTime = Math.round(response.data.sys.sunrise);
 let dateRise = new Date(sunriseTime * 1000);
@@ -63,6 +64,7 @@ let hoursSet = String(dateSet.getHours()).padStart(2, "0");
 let minutesSet = String(dateSet.getMinutes()).padStart(2, "0");
 let sunsetEL = document.querySelector("#details-sunset");
 
+cityEl.innerHTML = `${city}`;
 tempEl.innerHTML = Math.round(response.data.main.temp);
 tempRealEl.innerHTML = Math.round(response.data.main.feels_like);
 windEl.innerHTML = Math.round(response.data.wind.speed * 3.6);
@@ -71,10 +73,15 @@ sunriseEL.innerHTML = `${hoursRise} : ${minutesRise}`;
 sunsetEL.innerHTML = `${hoursSet} : ${minutesSet}`;
 timeEl.innerHTML = formatDate(response.data.dt * 1000);
 dayEl.innerHTML = fullDate();
+iconEl.setAttribute(
+"src", 
+`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+
+
 }
 
 let apiKey = "b5fbcef1543bc4503e1a5412457235aa";
-let city = "Barcelona";
+let city = "Lisbon";
 let units = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
